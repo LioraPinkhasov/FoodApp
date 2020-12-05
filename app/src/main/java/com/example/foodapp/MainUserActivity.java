0f2f3e1e2e320bfd417727f945a1f675c09cb5eb
RoieHub <roiemalykin@gmail.com>
2020-12-05 18:28:57 +0200

    added "add recipe" button and activity ,  connected from user main

--- app/src/main/java/com/example/foodapp/MainUserActivity.java
+++ app/src/main/java/com/example/foodapp/MainUserActivity.java
@@ -17,6 +17,13 @@ public class MainUserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_user);
     }
 
+    public void addRecipe(View view)
+    {
+        startActivity(new Intent(getApplicationContext(),addRecipe.class)); // Send the user to the rigister/login activity
+        finish();
+
+    }
+
     public void logout(View view)
     {
         FirebaseAuth.getInstance().signOut(); // Logout of a user
