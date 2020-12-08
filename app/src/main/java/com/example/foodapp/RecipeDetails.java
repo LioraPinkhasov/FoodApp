9de75e1d7afea654fcd1a1383e4ddf4735158de0
RoieHub <roiemalykin@gmail.com>
2020-12-08 12:52:05 +0200

    RecipeDetails and RecipeHeader getting readdy to get data from DB

--- app/src/main/java/com/example/foodapp/RecipeDetails.java
+++ app/src/main/java/com/example/foodapp/RecipeDetails.java
@@ -13,30 +13,89 @@ public class RecipeDetails
     // Memebrs as defined in FbDb
 
     // A : id
-    public  String id;
+    private   String A;
 
     // B : the products used in the recipe
 
-    public List<Ingredient> ingredients;
+    private String B;
 
     // C : Units to use for each ingredient in B
 
-    public List<String> units;
+    private String C;
 
-    ///// Time members are not yet implemented
+    // D : Creation date
+
+    private String D;
+    // E : Update date
+    private String E;
 
     // Default constructor
     public RecipeDetails() {}
 
     // More ditailed constructor
-    public RecipeDetails( String id , List<Ingredient> ingredients , List<String> units )
+    public RecipeDetails( String id ,String ingredients , String units , String create_date , String update_date )
     {
-        this.id = id;
-        this.ingredients = ingredients;
-        this.units = units;
+        this.A = id;
+        this.B = ingredients;
+        this.C = units;
+        this.D = create_date;
+        this.E = update_date;
     }
 
+    /**
+     * getA = getID the name is for DB
+     * @return id
+     */
+    public String getA()
+    {
+        return A;
+    }
 
+    /**
+     * setA = setID
+     * @param A
+     */
+    public void setA(String id)
+    {
+        this.A = id;
+    }
+
+    /**
+     * SetB = setName
+     * @param
+     */
+    public String getB()
+    {
+        return B;
+    }
+    public void setB(String name)
+    {
+        this.B = name;
+    }
+    public String getC()
+    {
+        return C;
+    }
+    public void setC(String units)
+    {
+        this.C = units;
+    }
+    public  String getD()
+    {
+        return D;
+    }
+    public void setD(String create_date)
+    {
+        this.D = create_date;
+    }
+
+    public String getE() {
+        return E;
+    }
+
+    public void setE(String e) {
+        E = e;
+    }
     // This next part is a constructor by id ,  where it will find the proper recipe from out DB and fill the details according to it.
 
     public RecipeDetails (String id)
--- app/src/main/java/com/example/foodapp/RecipeHeader.java
+++ app/src/main/java/com/example/foodapp/RecipeHeader.java
@@ -7,26 +7,69 @@ public class RecipeHeader
 {
     // Members
 
-    // A : recipe id; is the same as the ID in RecipeDetails
-    public String id;
 
+    private String A,B,C,D,E;
+    // A : recipe id; is the same as the ID in RecipeDetails
     // B : Hebrew name not implemented
     // C : English name
-    public String name;
+    //public String name;
 
     // Default constructor
     public RecipeHeader() {}
     // Explicit constructor
-    public RecipeHeader(String id , String name )
+    public RecipeHeader(String a , String b , String c , String d , String e )
     {
-
+        this.A = a;
+        this.B = b;
+        this.C = c;
+        this.D = d;
+        this.E = e;
     }
 
+    public String getA() {
+        return A;
+    }
+
+    public void setA(String a) {
+        A = a;
+    }
+
+    public String getB() {
+        return B;
+    }
+
+    public void setB(String b) {
+        B = b;
+    }
+
+    public String getC() {
+        return C;
+    }
+
+    public void setC(String c) {
+        C = c;
+    }
+
+    public String getD() {
+        return D;
+    }
+
+    public void setD(String d) {
+        D = d;
+    }
+
+    public String getE() {
+        return E;
+    }
+
+    public void setE(String e) {
+        E = e;
+    }
     /////////////// This constructor contacts the DB and pass the RecipeHeader info to create an instance of RecipeHeader \\\\\\\\\\\\\
 
     // Connect to FBDB
-   FirebaseDatabase database = FirebaseDatabase.getInstance() ;
-    DatabaseReference dbRHRef = database.getReference() ;
+  // FirebaseDatabase database = FirebaseDatabase.getInstance() ;
+   // DatabaseReference dbRHRef = database.getReference() ;
 
 
 
