f1f3b7639dd727588fa324a838ce90bdec5c02f5
Joseph-Schtein <yossiand10@gmail.com>
2020-12-08 15:43:10 +0200

    starting implementing search

--- app/src/main/java/com/example/foodapp/Search.java
+++ app/src/main/java/com/example/foodapp/Search.java
@@ -1,13 +1,14 @@
-
-
 package com.example.foodapp;
 
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.MultiAutoCompleteTextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -18,24 +19,26 @@ import com.google.firebase.database.ValueEventListener;
 public class Search extends AppCompatActivity {
 
     private MultiAutoCompleteTextView needToFind;
-
-
+    private List<Ingredient> ingredientList;
+    private Query query;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        ingredientList = new ArrayList<>();
         needToFind = (MultiAutoCompleteTextView)findViewById(R.id.insert_data_space);
+        ArrayAdapter<Ingredient> ingredientAdapter = new ArrayAdapter<Ingredient>(this, android.R.layout.simple_list_item_1,ingredientList);
+        needToFind.setAdapter(ingredientAdapter);
 
-
-
-
+        //query =
 
 
     }
+
+
 }
 
 
