880682fe928e08989307a41806c1b7cb29144f37
RoieHub <roiemalykin@gmail.com>
2020-12-17 14:34:00 +0200

    add Recipe is now adding recipes

--- app/src/main/java/com/example/foodapp/addRecipe.java
+++ app/src/main/java/com/example/foodapp/addRecipe.java
@@ -1,22 +1,41 @@
 package com.example.foodapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class addRecipe extends AppCompatActivity
 {
-
+    // Views decleration
     Button sendRecipe;
     Button addPhoto;
     EditText recipeName;
     EditText ingridientsList;
     EditText howToDescription;
+
+
+    // DB decleration
+
+    FirebaseDatabase mDatabase;
+    DatabaseReference dbRecipeRef;
+    private FirebaseAuth mAuth;
+
+
+
 
 
     @Override
@@ -33,14 +52,21 @@ public class addRecipe extends AppCompatActivity
         ingridientsList = (EditText)findViewById(R.id.ingredient_list_text);
         howToDescription = (EditText)findViewById(R.id.wrkProgress_text);
 
+        // Init DB references
+        mDatabase = FirebaseDatabase.getInstance();
+        dbRecipeRef = mDatabase.getReference().child("RecpieDetiels");
+        // Initing FirebaseAuth instance
+        mAuth = FirebaseAuth.getInstance();
+
         // Clicking sendRecipe
         sendRecipe.setOnClickListener(new View.OnClickListener() {
+        @RequiresApi(api = Build.VERSION_CODES.O)
         @Override
         public void onClick(View v)
         {
             // Parts of recipe
-            String rName = recipeName.getText().toString().trim(); // Should I use trim?
-            String rIngridients = ingridientsList.getText().toString().trim();
+            String rName = recipeName.getText().toString().toLowerCase().trim(); // Should I use trim?
+            String rIngridients = ingridientsList.getText().toString().toLowerCase().trim();
             String howTo = howToDescription.getText().toString().trim();;
 
             // 1) Check if any field is empty and show toast ,  else put them inside a string;
@@ -70,18 +96,29 @@ public class addRecipe extends AppCompatActivity
                 if(rIngridients.charAt(i) == ',') commas++;
             }
             // 2.b ) Establish current Date
-            String currentDate = "00/00/00";
-            // 2.c ) How to make an ID?
-            String id = "What is id used for?";
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            LocalDateTime now = LocalDateTime.now();
+            String currentDate = now.toString();
+            // 2.c ) User id is given by DBfirebase and I update in when putting the recipe in DB
+            String id = "";
             // 2.d) Extract and put user email in the host string
-            String host = "how to get the email of a user?";
+            FirebaseUser currentUser = mAuth.getCurrentUser(); // getting user Info from Authentication system
+            String host = currentUser.getEmail(); // host is now our email.
             // 2.e)
             String measures = "How do I get the measures?";
-
-            Recipe newRecipe = new Recipe( 0 , currentDate ,host,howTo ,id ,measures , commas+1 ,rIngridients , rName );
-
-
             // 3) Store the recipe in DB
+            Recipe newRecipe = new Recipe( 0 , currentDate ,host,howTo ,id ,measures , commas+1 ,rIngridients , rName ); // Creating the new recipe
+            newRecipe.setId(dbRecipeRef.push().getKey());
+            dbRecipeRef.child(newRecipe.getId()).setValue(newRecipe);
+            // 4) Toast and move to main page.
+            Toast.makeText(addRecipe.this, "Thank you ! your recipe is added ! and will be visible when aproved by admin. ", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(getApplicationContext(), MainUserActivity.class)); // This is the proper path!
+
+
+
+
+
+
 
         }
 
