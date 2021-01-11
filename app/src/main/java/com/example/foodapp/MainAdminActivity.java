8a4e5934dacd930c5d7ca60f68589cb64945bd68
op1995 <pellerofir@gmail.com>
2021-01-11 11:16:51 +0200

    Fixed unapproved recipes button from admin options activity (caused crash) TODO - implement what clicking that button does (onClickListener)
    - Ofir

--- app/src/main/java/com/example/foodapp/MainAdminActivity.java
+++ app/src/main/java/com/example/foodapp/MainAdminActivity.java
@@ -3,12 +3,28 @@ package com.example.foodapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainAdminActivity extends AppCompatActivity {
+
+    private Button button_unapproved_recipes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_admin);
+
+        button_unapproved_recipes = (Button) findViewById(R.id.button_unapproved_recipes);
+
+        button_unapproved_recipes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+
     }
 }
