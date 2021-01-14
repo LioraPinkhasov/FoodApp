44a6678e4b276a49d78046d29a044fa97f1874a5
RoieHub <roiemalykin@gmail.com>
2021-01-14 17:23:07 +0200

    this

--- app/src/main/java/com/example/foodapp/Recpie_Admin_permmision.java
+++ app/src/main/java/com/example/foodapp/Recpie_Admin_permmision.java
@@ -5,11 +5,15 @@
 
 package com.example.foodapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -170,4 +174,27 @@ public class Recpie_Admin_permmision extends AppCompatActivity
 
     
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.basic_menu, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.item1:
+                startActivity(new Intent(getApplicationContext(), Contact_us.class));
+                return true;
+            
+            case R.id.item2:
+                startActivity(new Intent(getApplicationContext(), register_login.class));
+                return true;
+            
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    
 }
